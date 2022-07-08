b23048708cf55cfdf21f32e7c54a90d3705103d5
Atticus Zambrana <atticus@kustomcakes.net>
2022-07-07 23:23:57 -0700

    Basic MySQL Connectivity working

--- src/main/java/com/thecloudyco/ditto/Starter.java
+++ src/main/java/com/thecloudyco/ditto/Starter.java
@@ -1,12 +1,15 @@
 package com.thecloudyco.ditto;
 
 import com.thecloudyco.ditto.cmd.CommandCenter;
+import com.thecloudyco.ditto.database.CoreDatabase;
+import com.thecloudyco.ditto.util.LogUtil;
 import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 
 import javax.security.auth.login.LoginException;
+import java.sql.SQLException;
 
 public class Starter {
 
@@ -18,6 +21,14 @@ public class Starter {
         discord = JDABuilder.createDefault(dotenv.get("DISCORD_TOKEN"));
         discord.setBulkDeleteSplittingEnabled(false);
         discord.setActivity(Activity.watching("you sleep"));
+
+        // Setup the connection to the MySQL Database
+        try {
+            CoreDatabase.getInstance().connect();
+        } catch (SQLException e) {
+            LogUtil.severe("Unable to communicate with the database! Program has to shut down now...");
+            System.exit(1);
+        }
 
         // Get all of the pre-built commands in mem and registered to the api
         CommandCenter.registerCommands();
--- src/main/java/com/thecloudyco/ditto/database/CoreDatabase.java
+++ src/main/java/com/thecloudyco/ditto/database/CoreDatabase.java
@@ -1,7 +1,69 @@
 package com.thecloudyco.ditto.database;
 
+import com.thecloudyco.ditto.util.LogUtil;
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class CoreDatabase {
     /***
      * Singleton Class for managing all the code related to communication with the MySQL Database
+     * @Author: Atticus Zambrana
      */
+    private static CoreDatabase me;
+    public static CoreDatabase getInstance() {
+        if(me == null) {
+            me = new CoreDatabase();
+        }
+        return me;
+    }
+
+    private boolean isConnected;
+    private int disconnectTimer;
+    private String queue;
+
+    private String myHostname;
+    private String myDatabase;
+    private String myUsername;
+    private String myPassword;
+
+    private Connection connection = null;
+
+    public CoreDatabase() {
+        // Pull any config from the .env file, and set it in this instance
+        Dotenv dotenv = Dotenv.load();
+        this.myHostname = dotenv.get("MYSQL_HOSTNAME");
+        this.myDatabase = dotenv.get("MYSQL_DATABASE");
+        this.myUsername = dotenv.get("MYSQL_USERNAME");
+        this.myPassword = dotenv.get("MYSQL_PASSWORD");
+        LogUtil.info("Database setup complete. Ready to transact!");
+    }
+
+    public void connect() throws SQLException {
+        if(isConnected) return;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            LogUtil.severe("MySQL Connection Class NOT FOUND! Shutting down application...");
+            System.exit(1);
+        }
+
+        connection = DriverManager.getConnection(
+                "jdbc:mysql://" + myHostname + ":3306/" + myDatabase,
+                myUsername, myPassword);
+    }
+
+    public void disconnect() {
+
+    }
+
+    public boolean isConnected () {
+        return isConnected;
+    }
+
+    public int getDisconnectTimer() {
+        return disconnectTimer;
+    }
 }
